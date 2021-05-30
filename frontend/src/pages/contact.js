ea201c658c805e09acc4d00bea44af168b7a523d
Wiktor Grzejszczyk <wiktor@grzejszczyk.com>
2021-05-30 11:54:23 +0200

    add googlemaps button

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -23,10 +23,9 @@ const ContactPage = (props) => {
           <span>tel. <a href="tel:000000000">000 000 000</a></span>
         </address>
         <address className="contactAddress">
-          <a href="https://www.google.com/maps/place/O%C5%9Brodkowa+2,+66-004+Drzonk%C3%B3w,+Polska/@51.8996495,15.5651912,139m/data=!3m1!1e3!4m8!1m2!2m1!1sul.+O%C5%9Brodkowa+2+Zielona+G%C3%B3ra+-+Drzonk%C3%B3w!3m4!1s0x470611dc3fb813d5:0xf53d4db7e30070f6!8m2!3d51.899855!4d15.5656311">
-            <span>ul. Środkowa 2</span>
-            <span>Zielona Góra - Drzonków</span>
-          </a>
+          <span>ul. Środkowa 2</span>
+          <span>Zielona Góra - Drzonków</span>
+          <a className="btn-tertiary" href="https://www.google.com/maps/place/O%C5%9Brodkowa+2,+66-004+Drzonk%C3%B3w,+Polska/@51.8996495,15.5651912,139m/data=!3m1!1e3!4m8!1m2!2m1!1sul.+O%C5%9Brodkowa+2+Zielona+G%C3%B3ra+-+Drzonk%C3%B3w!3m4!1s0x470611dc3fb813d5:0xf53d4db7e30070f6!8m2!3d51.899855!4d15.5656311">Show on Google Maps</a>
         </address>
       </div>
     </Layout>
